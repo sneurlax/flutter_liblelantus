056db9102e2f7c33eb9a564b1caab228c6ae4f34
sneurlax <sneurlax@gmail.com>
2022-10-27 16:23:24 -0500

    stash work

--- scripts/crosscompile/build_sharedfile.sh
+++ scripts/crosscompile/build_sharedfile.sh
@@ -11,6 +11,6 @@ COMMIT=$(git log -1 --pretty=format:"%H")
 sed -i "/\/\*${OS}_VERSION/c\\/\*${OS}_VERSION\*\/ const ${OS}_VERSION = \"$COMMIT\";" $VERSIONS_FILE
 cd build
 rm CMakeCache.txt
-cmake ./mobileliblelantus/ -DCMAKE_TOOLCHAIN_FILE=/home/user/src/flutter_liblelantus/scripts/crosscompile/CMakeLists/Toolchain-cross-mingw32-linux.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo
+cmake ./mobileliblelantus/ -DCMAKE_TOOLCHAIN_FILE=/home/user/src/flutter_liblelantus/scripts/crosscompile/CMakeLists/Toolchain-cross-mingw32-linux.cmake -DCMAKE_BUILD_TYPE=RelWithDebInfo -lws2_32
 make -j$(nproc)
 make install
